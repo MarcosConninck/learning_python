bbae467caf5855d8a1b0c4757274b4816c425fad
MarcosConninck <105248672+MarcosConninck@users.noreply.github.com>
2024-04-29 18:09:01 -0300

    Update grafos_hash_set.py
    adicionado algoritmo completo de pesquisa em largura, com opção de escolher quem é o vendedor

--- grafos_hash_set.py
+++ grafos_hash_set.py
@@ -44,23 +44,42 @@ grafo["jonny"] = []
 # 6º caso a fila esteja vazia, não há vendedores
 
 
-fila_de_pesquisa: deque = deque()
-fila_de_pesquisa += grafo["eu"]
-
-
-def pessoa_vendedora(pessoa):
-    if pessoa == "anuj":
+def pessoa_vendedora(pessoa: str) -> bool:
+    if pessoa == vendedor:
         return True  # sim! pronto
-    else:
-        return False  # não! adiciona os vizinhos dela a fila
+    return False  # não! adiciona os vizinhos dela a fila
 
 
-while fila_de_pesquisa:  # enquanto a fila não estiver vazia
-    pessoa = fila_de_pesquisa.popleft()  # pega primeira pessoa da fila
-    if pessoa_vendedora(pessoa) is True:  # conferindo se a pessoa é vendedora
-        print(f'{pessoa} é vendedor de manga')
-        break
-    elif fila_de_pesquisa == deque([]):
-        print("não há vendedores de manga")
-    else:
-        fila_de_pesquisa += grafo[pessoa]  # chama o loop novamente
+def pesquisa(nome: str) -> bool:
+    print('Pessoas na lista:')
+    for n in grafo:
+        print(f'-> {n}')
+    fila_de_pesquisa: deque = deque()
+    fila_de_pesquisa += grafo[nome]
+    # Mantêm as pessoas verificadas em uma lista, para não entrar em loop inf
+    verificadas = []
+    # enquanto a fila não estiver vazia
+    while fila_de_pesquisa:
+        # pega primeira pessoa da fila
+        pessoa = fila_de_pesquisa.popleft()
+        print(f'verificando: {pessoa}')
+        if pessoa not in verificadas:
+            # conferindo se a pessoa é vendedora
+            if pessoa_vendedora(pessoa) is True:
+                print(f'{pessoa} é vendedor de manga')
+                return True
+            # chama o loop novamente
+            else:
+                # adiciona em ordem novas pessoas a serem verificadas
+                fila_de_pesquisa += grafo[pessoa]
+                verificadas.append(pessoa)
+
+        elif fila_de_pesquisa == deque([]):
+            print("não há vendedores de manga, todos foram verificados")
+
+    return False
+
+
+print(grafo)
+vendedor = input('Determine o nome do vendedor: ').lower()
+pesquisa('eu')
