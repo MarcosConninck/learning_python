3249f3e975d7c46a25d754975726903721f894c3
marcos <git.conninck@gmail.com>
2024-05-16 18:51:21 -0300

    update dijkstra_algorithm.py

--- exemplos/dijkstra_algorithm.py
+++ exemplos/dijkstra_algorithm.py
@@ -1,56 +1,46 @@
-"""
-Este algorimo, em específico, é similar a pesquisa em largura, porém leva em
-conta o "peso" de cada trajeto, de forma ponderada.
-
-O algoritmo possui 4 etapas:
-    -> Encontre o vertice mais "barato". Este é o vértice em que você consegue
-    chegar no menor tempo possível.
-    -> Atualize o custo dos vizinhos deste vértice.
-    -> Repita até que você tenha feito isso para cada vértice do grafo. (loop)
-    -> Calcule o caminho final
-"""
 grafo: dict = {}
 grafo['inicio'] = {}
-grafo['inicio']['a'] = 6
+grafo['inicio']['a'] = 5
 grafo['inicio']['b'] = 2
 grafo['a'] = {}
-grafo['a']['fim'] = 1
+grafo['a']['c'] = 4
+grafo['a']['d'] = 2
 grafo['b'] = {}
-grafo['b']['a'] = 3
-grafo['b']['fim'] = 5
+grafo['b']['a'] = 8
+grafo['b']['d'] = 7
+grafo['c'] = {}
+grafo['c']['d'] = 6
+grafo['c']['fim'] = 3
+grafo['d'] = {}
+grafo['d']['fim'] = 1
 grafo['fim'] = {}
 print('grafo ', grafo)
 
 infinito = float('inf')
 custos: dict = {}
-custos['a'] = 6
+custos['a'] = 5
 custos['b'] = 2
+custos['c'] = infinito
+custos['d'] = infinito
 custos['fim'] = infinito
 print('custos ', custos)
 
 pais: dict = {}
 pais['a'] = 'inicio'
 pais['b'] = 'inicio'
+pais['c'] = 'a'
+pais['d'] = 'a', 'b', 'c'
 pais['fim'] = None
 print('pais ', pais)
 
 processados: list = []
 print('processados ', processados)
 
-"""
-enquanto houver grafos a serem processados...
-while processados is not None:
-pegue o vértice que está mais proximo do início...
-atualize os custos para seus vizinhos...
-se qualquer um dos custos dos vizinhos for atualizado, atualize tb o pai...
-marque o vértice como processado...
-repita
-"""
 
-# encontra o custo mais baixo que ainda não foi processado
 print('\n#########################\n')
 
 
+# encontra o custo mais baixo que ainda não foi processado
 def ache_no_custo_mais_baixo(custos):
     custo_mais_baixo = float('inf')
     vertice_custo_mais_baixo = None
@@ -59,7 +49,7 @@ def ache_no_custo_mais_baixo(custos):
         if custo < custo_mais_baixo and vertice not in processados:
             custo_mais_baixo = custo
             vertice_custo_mais_baixo = vertice
-    if vertice_custo_mais_baixo is not None:
+    if vertice_custo_mais_baixo is not None and custo_mais_baixo <= custo:
         print(f'vertice {vertice_custo_mais_baixo}, custo {custo_mais_baixo}')
     return vertice_custo_mais_baixo
 
@@ -79,12 +69,15 @@ while vertice is not None:
             custos[n] = novo_custo
             # este vertice se torna o novo pai para o vizinho
             pais[n] = vertice
+            print(f'custo dos vizinhos: {vizinhos[n]}')
     # marca o vertice como processado, adicionando-o a lista processados
     processados.append(vertice)
-    print(processados)
-    # encontra o próximo vértice a ser processado e o algoritmo é repetido.
-    print(f'caminho: {ciclo}°\n')
+    if len(processados) < len(custos):
+        print(f'analisados: {processados}')
+    if 'fim' not in processados:
+        print(f'ciclo: {ciclo}°\n')
     ciclo += 1
+    # encontra o próximo vértice a ser processado e o algoritmo é repetido.
     vertice = ache_no_custo_mais_baixo(custos)
 
 
